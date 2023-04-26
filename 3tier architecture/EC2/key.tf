c7625035528e1ae7e1e2a42c4a4ee3752b7db00c
tkdemd136195 <122110276+tkdemd136195@users.noreply.github.com>
2023-04-26 19:49:13 +0900

    key file

--- 3tier architecture/EC2/key.tf
+++ 3tier architecture/EC2/key.tf
@@ -1,17 +1,17 @@
 #키파일 생성
 # Create .pem key file in local
-resource "tls_private_key" "TEST" {
+resource "tls_private_key" "aws" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "TEST" {
-  key_name   = "TEST"
-  public_key = tls_private_key.TEST.public_key_openssh
+resource "aws_key_pair" "aws" {
+  key_name   = "aws"
+  public_key = tls_private_key.aws.public_key_openssh
 }
 
 
 resource "local_file" "ssh_key" {
-  filename = "TEST.pem"
-  content = tls_private_key.TEST.private_key_pem
+  filename = "aws.pem"
+  content = tls_private_key.aws.private_key_pem
 }

2c3e3c9e87f58168d763f5b3598b27be59cdb020
tkdemd136195 <122110276+tkdemd136195@users.noreply.github.com>
2023-04-26 17:50:16 +0900

    web.sh

--- 3tier architecture/web.sh
+++ 3tier architecture/web.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-    # UserData for Amazon Linux 2023
+# UserData for Amazon Linux 2023
     
-    # Update system packages
-    yum update -y
+# Update system packages
+yum update -y
 
-    # Install Nginx
-    yum install nginx -y
+# Install Nginx
+yum install nginx -y
 
-    # Start Nginx service
-    systemctl start nginx
+# Start Nginx service
+systemctl start nginx
 
-    # Enable Nginx service to start on boot
-    systemctl enable nginx
+# Enable Nginx service to start on boot
+systemctl enable nginx
 
-    # Web Server Document Root Path
-    document_root="/usr/share/nginx/html"
+# Web Server Document Root Path
+document_root="/usr/share/nginx/html"
